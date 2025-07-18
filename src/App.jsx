e4450f44822492c0435888f5300c3d02cc5f9b7a
Deepesh Gaharwar <deepeshgaharwar3@gmail.com>
2025-07-18 21:22:53 +0530

    config routing & added Header, Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from "react"
+import NavBar from "./components/NavBar"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Body from "./components/Body"
 
 function App() {
 
   return (
-    <>
-       <h1 className='bg-slate-600 text-cyan-100'> App </h1>
+    <> 
+      
+      <BrowserRouter basename="/">
+
+         <Routes>
+
+            <Route path="/" element = { <Body/> }> 
+            
+            
+            
+            </Route>
+             
+         </Routes>
+
+      </BrowserRouter>
 
     </>
   )
